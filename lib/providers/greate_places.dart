b4022693e945ebd47385c28ba37a362804dd2421
Gabriel Davi <gdavi.gd@gmail.com>
2020-12-08 07:35:56 -0300

    place model and provider

---
+++ lib/providers/greate_places.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+import 'package:greatPlaces/models/place.dart';
+
+class GreatePlaces with ChangeNotifier {
+  List<Place> _items = [];
+
+  List<Place> get items {
+    return [..._items];
+  }
+
+  int get itemsCount {
+    return _items.length;
+  }
+
+  Place itemByIndex(int index) {
+    return _items[index];
+  }
+}
