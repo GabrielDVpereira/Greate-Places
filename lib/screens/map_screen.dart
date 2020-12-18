34f032ecab63ecbc7c271143150771a33ef32c8e
Gabriel Davi <gdavi.gd@gmail.com>
2020-12-18 18:26:26 -0300

    putting marker on the map

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -4,18 +4,26 @@ import 'package:greatPlaces/models/place.dart';
 
 class MapScreen extends StatefulWidget {
   final PlaceLocation initialLocation;
-
+  final bool isReadOnly;
   MapScreen({
     this.initialLocation = const PlaceLocation(
       latitude: 37.419857,
       longitude: -122.078827,
     ),
+    this.isReadOnly = false,
   });
   @override
   _MapScreenState createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng _pickedPosition;
+  void _selectPosition(LatLng position) {
+    setState(() {
+      _pickedPosition = position;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +38,12 @@ class _MapScreenState extends State<MapScreen> {
           ),
           zoom: 13,
         ),
+        onTap: widget.isReadOnly ? null : _selectPosition,
+        markers: _pickedPosition == null
+            ? null
+            : {
+                Marker(markerId: MarkerId('p1'), position: _pickedPosition),
+              },
       ),
     );
   }
