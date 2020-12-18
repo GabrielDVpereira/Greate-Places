2408dcdc9a301371b83ddf1e2f9a0cee80167156
Gabriel Davi <gdavi.gd@gmail.com>
2020-12-18 18:43:06 -0300

    Saving position from the map

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -29,6 +29,17 @@ class _MapScreenState extends State<MapScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Selecione...'),
+        actions: [
+          if (!widget.isReadOnly)
+            IconButton(
+              icon: Icon(Icons.check),
+              onPressed: _pickedPosition == null
+                  ? null
+                  : () {
+                      Navigator.of(context).pop(_pickedPosition);
+                    },
+            )
+        ],
       ),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:greatPlaces/screens/map_screen.dart';
 import 'package:greatPlaces/utils/location_util.dart';
 import 'package:location/location.dart';
@@ -22,14 +23,16 @@ class _LocationInputState extends State<LocationInput> {
   }
 
   Future<void> _selectOnMap() async {
-    final selectedLocation = await Navigator.of(context).push(
+    final LatLng selectedPosition = await Navigator.of(context).push(
       MaterialPageRoute(
         fullscreenDialog: true,
         builder: (ctx) => MapScreen(),
       ),
     );
 
-    if (selectedLocation == null) return;
+    if (selectedPosition == null) return;
+
+    print(selectedPosition);
   }
 
   @override
